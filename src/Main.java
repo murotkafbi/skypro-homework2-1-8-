4024fbeecfc6375eec965e10dae639fac29535ef
unik0 <murotkafbi@gmail.com>
2022-12-23 18:53:32 +0300

    homework

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+public class    Main {
+ public static void main(String[] args) {
+  var dog = 8.0;
+  System.out.println(dog);
+  var cat = 3.6;
+  System.out.println(cat);
+  var paper = 763789;
+  System.out.println(paper);
+  dog = dog + 4;
+  System.out.println(dog);
+  cat = cat + 4;
+  System.out.println(cat);
+  paper = paper + 4;
+  System.out.println(paper);
+  dog = dog - 3.5;
+  System.out.println(dog);
+  cat = cat - 1.6;
+  System.out.println(cat);
+  paper = paper - 7639;
+  System.out.println(paper);
+  var friend = 19;
+  System.out.println(friend);
+  friend = friend * 2;
+  System.out.println(friend);
+  friend = friend / 7;
+  System.out.println(friend);
+  var frog = 3.5;
+  System.out.println(frog);
+  frog = frog * 10;
+  System.out.println(frog);
+  frog = frog / 3.5;
+  System.out.println(frog);
+  frog = frog + 4;
+  System.out.println(frog);
+  var bokser1 = 78.2;
+  var bokser2 = 82.7;
+  var generalWeight = bokser1 + bokser2;
+  System.out.println("общий вес " + generalWeight + "кг!");
+  var difference = bokser2 - bokser1;
+  System.out.println(difference);
+ generalWeight=bokser1 / bokser2;
+  System.out.println("Разница между боксерами" + generalWeight + "кг!");
+  var hoursWork = 640;
+  var timeWork = 8;
+  var all = hoursWork / timeWork;
+  System.out.println("Всего работников в компании" + all + "человек");
+ var hours= 94;
+  hoursWork=hoursWork/hours;
+  System.out.println("Если в копании рабтает" + all + "человек,то всего "+hoursWork +"часов работы может быть поделено между сотрудниками");
+ }
+}
