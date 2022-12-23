3ab668dca25aed66386af30d2f424a2d78fcfaa6
murotkafbi <120704481+murotkafbi@users.noreply.github.com>
2022-12-23 19:22:41 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,39 @@
 public class    Main {
  public static void main(String[] args) {
+  // Задача 1
+  System.out.println("задача 1");
   var dog = 8.0;
   System.out.println(dog);
   var cat = 3.6;
   System.out.println(cat);
   var paper = 763789;
   System.out.println(paper);
+  // задача 2
+  System.out.println("Задача 2");
   dog = dog + 4;
   System.out.println(dog);
   cat = cat + 4;
   System.out.println(cat);
   paper = paper + 4;
   System.out.println(paper);
+  // задача 3
+  System.out.println("Задача 3");
   dog = dog - 3.5;
   System.out.println(dog);
   cat = cat - 1.6;
   System.out.println(cat);
   paper = paper - 7639;
   System.out.println(paper);
+  //Задача 4
+  System.out.println("зачада 4");
   var friend = 19;
   System.out.println(friend);
-  friend = friend * 2;
+  friend = friend + 2;
   System.out.println(friend);
-  friend = friend / 7;
+  friend = friend / 7; // friend/ =7
   System.out.println(friend);
+  // Задача 5
+  System.out.println("Задача 5");
   var frog = 3.5;
   System.out.println(frog);
   frog = frog * 10;
@@ -32,20 +42,25 @@ public class    Main {
   System.out.println(frog);
   frog = frog + 4;
   System.out.println(frog);
+  // Задача 6
+  System.out.println("Задача 6");
   var bokser1 = 78.2;
   var bokser2 = 82.7;
-  var generalWeight = bokser1 + bokser2;
-  System.out.println("общий вес " + generalWeight + "кг!");
-  var difference = bokser2 - bokser1;
-  System.out.println(difference);
- generalWeight=bokser1 / bokser2;
-  System.out.println("Разница между боксерами" + generalWeight + "кг!");
+  System.out.println("общий вес двух бойцов:" + (bokser1+bokser2));
+  System.out.println("Разница весов дух бойцов:"+(bokser2-bokser1));
+  //Задача 7
+  System.out.println("Задача 7");
+  System.out.println("общий вес двух бойцов (1 Способ):" + (bokser1+bokser2));
+  System.out.println("Разница весов дух бойцов(2 Способ):"+(bokser2%bokser1));
+  // Задача 8
+  System.out.println("Задача 8");
+
   var hoursWork = 640;
   var timeWork = 8;
-  var all = hoursWork / timeWork;
-  System.out.println("Всего работников в компании" + all + "человек");
- var hours= 94;
-  hoursWork=hoursWork/hours;
-  System.out.println("Если в копании рабтает" + all + "человек,то всего "+hoursWork +"часов работы может быть поделено между сотрудниками");
+  var totalEmployees = hoursWork/timeWork;
+  System.out.println("Всего работников в компании:" + totalEmployees + "человек");
+  totalEmployees = totalEmployees+ 94;
+  hoursWork = totalEmployees * timeWork;
+  System.out.println("Если в копании рабтает:" + totalEmployees + "человек,то всего "+timeWork +"часов работы может быть поделено между сотрудниками");
  }
 }
